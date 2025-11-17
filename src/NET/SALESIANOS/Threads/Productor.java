4a77069a4206a5e236c5d0971a066403319b72de
Saidax277 <saidax2005@gmail.com>
2025-11-17 18:04:33 +0000

    Update Productor.java

--- src/NET/SALESIANOS/Threads/Productor.java
+++ src/NET/SALESIANOS/Threads/Productor.java
@@ -7,27 +7,31 @@ public class Productor extends Thread {
     private final Game tienda;
     private final String compania;
     private final int cantidad;
+    private final int tiempoMaxProduccion;
+
     private final String[] tiposDeJuegos = {
         "RPG", "Shooter", "Plataformas", "Puzzle", "Aventura",
         "Deportes", "Carreras", "Estrategia", "Simulación", "Lucha"
     };
 
     public Productor(Game tienda, String compania, int cantidad) {
+        this(tienda, compania, cantidad, 1000);
+    }
+
+    public Productor(Game tienda, String compania, int cantidad, int tiempoMaxProduccion) {
         this.tienda = tienda;
         this.compania = compania;
         this.cantidad = cantidad;
+        this.tiempoMaxProduccion = tiempoMaxProduccion;
     }
 
     @Override
     public void run() {
         try {
             for (int i = 1; i <= cantidad; i++) {
-                Thread.sleep((int)(Math.random() * 1000));
-
+                Thread.sleep((int)(Math.random() * tiempoMaxProduccion));
                 String tipo = tiposDeJuegos[(int)(Math.random() * tiposDeJuegos.length)];
-                
                 Videojuego juego = new Videojuego(i, tipo, compania);
-
                 tienda.guardar(juego);
             }
         } catch (InterruptedException e) {
